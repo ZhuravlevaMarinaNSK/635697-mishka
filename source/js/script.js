e9cf6dab2e038e26d664c6b91193002554490bb0
ZhuravlevaMarinaNSK <marina_july@mail.ru>
2018-04-30 01:03:01 +0600

    Правки по сеткам, контейнеры и элементы в потоке

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,8 @@
-<script>
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
+  var link = document.querySelector(".week-product__button");
+  var popup = document.querySelector(".modal");
+  var mask = document.querySelector(".overlay");
 
   navMain.classList.remove('main-nav--nojs');
 
@@ -14,4 +16,19 @@
       navMain.classList.remove('main-nav--opened');
     }
   });
-</script>
+
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal--show");
+    mask.classList.add("modal--show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+      if (popup.classList.contains("modal--show")) {
+    popup.classList.remove("modal--show");
+    mask.classList.remove("modal--show");
+      }
+    }
+  });
