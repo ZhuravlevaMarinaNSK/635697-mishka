7dbf06c05eb2375ba8da2efc608994b3b63e240d
ZhuravlevaMarinaNSK <marina_july@mail.ru>
2018-05-04 01:08:11 +0600

    Промежуточный ПП

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,12 @@
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
-  var link = document.querySelector(".week-product__button");
-  var popup = document.querySelector(".modal");
-  var mask = document.querySelector(".overlay");
+  var link = document.querySelector('.week-product__button');
+  var popup = document.querySelector('.modal');
+  var mask = document.querySelector('.overlay');
 
   navMain.classList.remove('main-nav--nojs');
 
   navToggle.addEventListener('click', function(evt) {
-    evt.preventDefault();
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
       navMain.classList.add('main-nav--opened');
@@ -17,18 +16,18 @@
     }
   });
 
-  link.addEventListener("click", function (evt) {
+  link.addEventListener('click', function (evt) {
     evt.preventDefault();
-    popup.classList.add("modal--show");
-    mask.classList.add("modal--show");
+    popup.classList.add('modal--show');
+    mask.classList.add('modal--show');
   });
 
-  window.addEventListener("keydown", function (evt) {
+  window.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 27) {
         evt.preventDefault();
-      if (popup.classList.contains("modal--show")) {
-    popup.classList.remove("modal--show");
-    mask.classList.remove("modal--show");
+      if (popup.classList.contains('modal--show')) {
+    popup.classList.remove('modal--show');
+    mask.classList.remove('modal--show');
       }
     }
   });
